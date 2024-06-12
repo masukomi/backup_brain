5acc8523e9d4b75373a40a8a67da97e13cac5445
masukomi <masukomi@masukomi.org>
2024-06-12 17:59:29 -0400

    link helpers enhanced & more
    - icon_link now supports optional text via 'text' keyword
    - added confirmation_icon_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,45 @@
 module ApplicationHelper
-  def icon_link(icon_name, url, link_css: nil, method: :get, alt: nil, title: nil)
+  def icon_link(icon_name, url,
+    link_css: nil,
+    method: :get,
+    alt: nil,
+    title: nil,
+    text: nil)
     link_to(
       image_tag(
         "/images/icons/#{icon_name}.svg"
-      ),
+      ) + icon_link_text(text),
       url,
       class: link_css,
       method: method,
       alt: (alt.presence || t("misc.missing_alt_text")),
       title: (title.presence || "")
     )
+  end
+
+  def confirmation_icon_link(icon_name, url,
+    link_css: nil,
+    method: :get,
+    alt: nil,
+    title: nil,
+    text: nil)
+    link_to(
+      image_tag(
+        "/images/icons/#{icon_name}.svg"
+      ) + icon_link_text(text),
+      url,
+      class: link_css,
+      method: method,
+      alt: (alt.presence || t("misc.missing_alt_text")),
+      title: (title.presence || ""),
+      data: {confirm: t("misc.are_you_sure")}
+    )
+  end
+
+  private
+
+  def icon_link_text(text)
+    return "" if text.blank?
+    "<span class='icon-link-text'>#{text}</span>".html_safe
   end
 end
