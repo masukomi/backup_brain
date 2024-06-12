a30fc3e0cca41a1a67ffea005827c563d2c4bbce
masukomi <masukomi@masukomi.org>
2024-06-12 18:00:36 -0400

    Bookmark has meilisearch & emoji integration now

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,18 +2,39 @@ class Bookmark
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  after_create :generate_archive
+  extend Search::ClassMethods
+  include Search::InstanceMethods
+  include BackupBrain::EmojiHelper
+
+  CLASS_PREFIXED_SEARCH_IDS  = true
+  SEARCHABLE_ATTRIBUTE_NAMES = %w[title url description tags created_at updated_at]
+  SEARCH_INDEX_NAME          = "backup_brain_general"
+
+  # Fields where it'll look for Slack-style emoji aliases
+  EMOJIFIABLE_FIELDS = [:description, :title]
 
   field :title,       type: String
   field :url,         type: String
   field :description, type: String
   field :tags,        type: Array
+  field :private,     type: Boolean, default: false
+  field :to_read,     type: Boolean, default: false
 
   embeds_many :archives
 
   validates :title, presence: true
   validates :url, presence: true
   validates :url, uniqueness: true
+
+  before_save  :emojify_default_fields
+  after_create :generate_archive
+
+  # enabled?() is controlled by the SEARCH_ENABLED environment variable
+  if Search::Client.instance.enabled?
+    after_create  :add_to_search
+    after_update  :update_in_search
+    after_destroy :remove_from_search
+  end
 
   def has_archive?
     archives.present?
@@ -30,7 +51,11 @@ class Bookmark
   end
 
   def generate_archive
-    raise t("errors.bookmarks.cant_archive_without_url") if url.blank?
+    if url.blank?
+      # only a warning because this shouldn't be a surprise.
+      Rails.logger.warning(t("errors.bookmarks.cant_archive_without_url"))
+      return
+    end
     ArchiveUrlJob.perform_now(self)
   end
 end
