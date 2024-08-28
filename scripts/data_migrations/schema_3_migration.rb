a90cae7b0e7d5f862abd8b660cc63ddc47e6ec72
masukomi <masukomi@masukomi.org>
2024-08-28 15:22:01 -0400

    added schema_3_migration

---
+++ scripts/data_migrations/schema_3_migration.rb
@@ -0,0 +1,35 @@
+require "paint"
+schema_version_setting = Setting.where(lookup_key: "schema_version").first
+if schema_version_setting&.value == 2
+  puts "Beginning migration to schema_version 3"
+
+  oauth2_client_id = Setting.where(lookup_key: "oauth2_client_id").first
+  if !oauth2_client_id
+    require "securerandom"
+
+    uuid = SecureRandom.uuid
+
+    # there shouldn't be one
+    Setting.create!(
+      lookup_key: "oauth2_client_id",
+      summary: "a unique oauth2 client id for this Backup Brain installation",
+      description: "Used when communicating with OAuth authenticated servers",
+      visible: false,
+      value: uuid
+    )
+    puts Paint["✅ New OAuth2 Client ID generated", :green]
+  else
+    warn Paint["an OAuth2 Client ID already exists.", :yellow]
+  end
+
+  schema_version_setting.value = 3
+  if schema_version_setting.save
+    puts Paint["✅ Updated schema_version setting to 3", :green]
+
+  else
+    warn Paint["⚠️  Unable to update schema_version setting to 3", :red]
+    exit 70 # EX_SOFTWARE (70) An internal software error has been detected.
+  end
+
+end
+exit 0 # no need to do anything
