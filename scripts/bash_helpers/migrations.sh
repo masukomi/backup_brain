900a0607f938f5f756dfe401b6e081662a2e13c8
masukomi <masukomi@masukomi.org>
2025-10-25 23:20:23 -0400

    updated serve and migrations.sh from forthcoming branch
    makes them work with systemd

--- scripts/bash_helpers/migrations.sh
+++ scripts/bash_helpers/migrations.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
+
+if [ -e "$HOME/.rbenv/shims/bundle" ]; then
+  PATH_TO_BUNDLE="$HOME/.rbenv/shims/bundle"
+else
+  PATH_TO_BUNDLE="bundle"
+fi
+set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/colors.sh
 
 function get_schema_version() {
-  schema_version=$(bundle exec rails runner "puts Setting.where(lookup_key: 'schema_version').first&.value" | tail -n1)
+  echo "about to get schema_version"
+  schema_version=$($PATH_TO_BUNDLE exec rails runner "puts Setting.where(lookup_key: 'schema_version').first&.value" | tail -n1)
 
   if [ "$schema_version" == "" ]; then
     schema_version=1
   fi
   echo $schema_version
 }
+
 function get_next_schema_version() {
     schema_version=$(get_schema_version)
-    let "schema_version=schema_version+1"
+    let schema_version+=1
     echo $schema_version
 }
 
