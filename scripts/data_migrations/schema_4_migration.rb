2f43a556b7e0383558f2344cfe132ab57fa729b9
masukomi <masukomi@masukomi.org>
2026-03-07 12:27:24 -0500

    schema_4_migration wasn't updating schema_version setting

--- scripts/data_migrations/schema_4_migration.rb
+++ scripts/data_migrations/schema_4_migration.rb
@@ -5,5 +5,13 @@ if schema_version_setting&.value == 3
   # load the new mastodon OauthSiteType data
   Rails.application.load_seed
 
+  schema_version_setting.value = 4
+  if schema_version_setting.save
+    puts Paint["✅ Updated schema_version setting to 4", :green]
+
+  else
+    warn Paint["⚠️  Unable to update schema_version setting to 4", :red]
+    exit 70 # EX_SOFTWARE (70) An internal software error has been detected.
+  end
 end
 exit 0 # no need to do anything
