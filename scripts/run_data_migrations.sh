b52db95e84b58a9eccf49dee0726d863dcb6934d
masukomi <masukomi@masukomi.org>
2026-03-31 16:13:12 -0400

    [add]       simple script to run data migrations directly
    scripts/run_data_migrations.sh
    Code Sections: migrations

---
+++ scripts/run_data_migrations.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+function shutdown() {
+  trap - INT TERM EXIT
+  echo "Shutting down"
+}
+
+trap shutdown INT TERM EXIT
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$(git -C "$SCRIPT_DIR" rev-parse --show-toplevel)
+source "$REPO_ROOT/scripts/bash_helpers/migrations.sh"
+run_next_migration_file_if_present
+
