f2b953ce998e3ef21243fbdaaa58669b2291f3a4
masukomi <masukomi@masukomi.org>
2026-03-31 16:15:14 -0400

    run_data_migrations.sh now runs bundle install first

--- scripts/run_data_migrations.sh
+++ scripts/run_data_migrations.sh
@@ -11,6 +11,7 @@ function shutdown() {
 trap shutdown INT TERM EXIT
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$(git -C "$SCRIPT_DIR" rev-parse --show-toplevel)
+bundle install
 source "$REPO_ROOT/scripts/bash_helpers/migrations.sh"
 run_next_migration_file_if_present
 
