acc2bc417cf953a400ddb2fdbec85b3d142df465
masukomi <masukomi@masukomi.org>
2026-03-31 20:08:01 -0400

    transcriptions now delete references to themselves in archive after destroy

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -20,4 +20,16 @@ class Transcription
 
   index({source_hash: 1, bookmark_id: 1}, {unique: true})
   index({bookmark_id: 1})
+
+  after_destroy :remove_from_archives
+
+  private
+
+  def remove_from_archives
+    Bookmark.where("archives.transcription_ids" => _id).each do |bookmark|
+      bookmark.archives.each do |archive|
+        archive.pull(transcription_ids: _id) if archive.transcription_ids.include?(_id)
+      end
+    end
+  end
 end
