f7278b9bc9f426f8574c0f8b8a9fb06f64381070
masukomi <masukomi@masukomi.org>
2026-04-02 18:36:17 -0400

    fixed safe params bug in settings controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -37,14 +37,16 @@ class SettingsController < ApplicationController
 
   # PATCH/PUT /settings/1 or /settings/1.json
   def update
+    cleaned_params = clean_params(@setting, setting_params)
     respond_to do |format|
-      if @setting.update(clean_params(setting_params))
-        helpers.bust_settings_cache
+      if @setting.update(cleaned_params)
         flash_message(:notice, I18n.t("settings.update_success"))
         format.html { redirect_to settings_url }
         format.json { render :show, status: :ok, location: @setting }
       else
-        flash_message(:error, I18n.t("settings.errors.update_error"))
+        @setting.errors.each do |error|
+          flash_message(:error, I18n.t("settings.errors.validation_error", attribute: error.attribute, problem: error.type))
+        end
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @setting.errors, status: :unprocessable_entity }
       end
@@ -77,21 +79,31 @@ class SettingsController < ApplicationController
     params.require(:setting).permit(:lookup_key, :summary, :description, :value, :value_type)
   end
 
-  def clean_params(params_hash)
-    cleaned_value = if params_hash[:value].present?
-      if @setting.is_boolean?
-        params_hash[:value] = params_hash[:value] == "true"
-      else
-        JSON.parse(params_hash[:value].strip)
-      end
-    end
-    params_hash[:value] = cleaned_value
+  def clean_params(setting, params_hash)
+    # # first the value type
+    value_type = params_hash[:value_type]
+    params_hash[:value_type] = value_type.present? ? value_type.to_sym : setting.value_type
 
+    # and then the value
+    raw_value = params_hash[:value]
+    params_hash[:value] = if raw_value.is_a?(ActionController::Parameters)
+      raw_value.to_unsafe_h.transform_keys(&:to_sym)
+    elsif raw_value.to_s.strip.present?
+      {value: JSON.parse(raw_value.to_s.strip)}
+    else
+      {value: nil}
+    end
     params_hash
+    # symbolified = {}
+    # params_hash.each do | key, val|
+    #   symbolified[key.to_sym] = val
+    # end
+    # symbolified
   end
 
   # 🤫 Sssshhhh is secret 1337 k0ntrol! No tell secret!
   def set_admin_enabled
-    @setting_administration_enabled = ENV.fetch("ENABLE_SETTINGS_ADMINISTRATION", "false") == "true"
+    false # FIXME fix handling of value_type before removing this
+    # @setting_administration_enabled = ENV.fetch("ENABLE_SETTINGS_ADMINISTRATION", "false") == "true"
   end
 end
